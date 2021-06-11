6bb57f41871bb046a3028376e6a88e8625d88146
lixworth <lixworth@gmail.com>
2021-06-11 21:15:02 +0800

    更新测试版本筛选 修复新特性

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,14 +9,32 @@
                 md="6"
                 lg="6"
             >
-                <v-card>
+                <v-card :loading="loading" :disabled="loading">
                     <v-card-title>
                         <span class="subtitle-1">选择下载版本</span>
                     </v-card-title>
                     <v-card-text>
+                        <v-radio-group
+                            v-model="type"
+                            row
+                        >
+                            <v-radio
+                                label="所有版本"
+                                value="all"
+                            ></v-radio>
+
+                            <v-radio
+                                label="正式版本"
+                                value="stable"
+                            ></v-radio>
+                            <v-radio
+                                label="测试版本"
+                                value="beta"
+                            ></v-radio>
+                        </v-radio-group>
                         <v-select
                             :items="versions"
-                            label="Solo field"
+                            :label="versions[0]"
                             solo
                         ></v-select>
                     </v-card-text>
@@ -34,29 +52,58 @@ export default {
     components: {
     },
     data: () => ({
-        versions: null,
-        items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+        versions: [
+            "正在拉取数据"
+        ],
+        type: "stable",
+        loading: false
     }),
     created() {
-        this.axios.get("/api").then((res)=>{
-            if(res.data.success){
-
-            }else{
-                this.get_cache();
-            }
-        }).catch((err) => {
-            this.get_cache();
-        });
+        this.pull_data();
+    },
+    watch:{
+        type(newVal,oldVal){
+            this.pull_data();
+        }
     },
     methods:{
+        pull_data(){
+            this.loading = true;
+            this.axios.get("/api").then((res)=>{
+                if(res.data.success){
+
+                }else{
+                    this.get_cache();
+                }
+            }).catch((err) => {
+                this.get_cache();
+            });
+        },
         get_cache(){
             this.axios.get("/data.json").then((res) => {
                 const data = res.data;
                 var versions_data = [];
                 data.forEach((item) => {
-                    versions_data.push(item.version);
+                    if(this.type == "all"){
+                        if(item.beta){
+                            versions_data.push(item.version+" [测试版]");
+                        }else{
+                            versions_data.push(item.version);
+                        }
+                    }else if(this.type == "stable"){
+                        if(!item.beta){
+                            versions_data.push(item.version);
+                        }
+                    }else if(this.type == "beta"){
+                        if(item.beta){
+                            versions_data.push(item.version);
+                        }
+                    }
+
                 });
                 this.versions = versions_data;
+                this.loading = false;
+
             })
         }
     }
